ed1c8fddc5c350ea49853d38b6ac58b0e20c422a
Devesh Dayal <devesh95@gmail.com>
2016-10-21 11:47:56 -0400

    reset for demo

--- js/collections/serviceCollection.js
+++ js/collections/serviceCollection.js
@@ -1,17 +1,4 @@
 $(function() {
   // TODO: define a service collection here
   
-  var ServiceList = Backbone.Collection.extend({
-
-    // Will hold objects of the Service model
-    model: window.Service,
-
-    // Return an array only with the checked services
-    getChecked: function(){
-      return this.where({checked:true});
-    }
-  });
-
-  window.ServiceList = ServiceList;
-  
 });
